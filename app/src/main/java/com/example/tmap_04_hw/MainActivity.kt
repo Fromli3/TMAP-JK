48cdce2373046f0f71610d67fe4a30ca16093f90
Fromli3 <jiri.kvapil03@gmail.com>
2024-03-21 13:50:41 +0100

    save01

--- app/src/main/java/com/example/tmap_04_hw/MainActivity.kt
+++ app/src/main/java/com/example/tmap_04_hw/MainActivity.kt
@@ -15,32 +15,35 @@ import com.example.tmap_04_hw.ui.theme.TMAP04HWTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            TMAP04HWTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TMAP04HWTheme {
-        Greeting("Android")
-    }
-}
+//            TMAP04HWTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    Greeting("Hello Android")
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    TMAP04HWTheme {
+//        Greeting("Android")
+//    }
+//}
